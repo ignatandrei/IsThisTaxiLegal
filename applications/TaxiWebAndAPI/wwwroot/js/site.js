b3658a9a53354dbd58e529adb814796d681c96e7
ignatandrei <ignatandrei@yahoo.com>
2018-01-07 19:02:18 +0200

    find the taxi

--- applications/TaxiWebAndAPI/wwwroot/js/site.js
+++ applications/TaxiWebAndAPI/wwwroot/js/site.js
@@ -1,1 +1,31 @@
-﻿// Write your JavaScript code.
+﻿function findTaxi(idPlateText, idResult) {
+
+    $("#" + idResult).text('please wait');
+    var plate = $("#" + idPlateText).val();
+    if (plate== null || plate.length == 0)
+        return;
+
+    var urlTaxi = "/api/Taxi/GetTaxi?plateNumber=" + plate;
+    $.ajax({
+        url: urlTaxi,
+        method: "GET"
+        })
+        .done(function (data) {
+            displayResult(data, idResult);
+        })
+        .fail(function (xhr) {
+            window.alert('error' + xhr);
+            displayResult(data, idResult);
+        });
+}
+function displayResult(data, idResult) {
+    
+    if (data === undefined) {
+        window.alert('no taxi');
+        ("#result").text('no taxi registered');
+    }
+    $("#result").text(JSON.stringify(data));
+
+}
+
+
