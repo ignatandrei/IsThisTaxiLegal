9f42ebcc22abfcd87d32cf7646cb44cc3a2c6c56
ignatandrei <ignatandrei@yahoo.com>
2018-02-18 11:34:30 +0200

    added id

--- applications/VersioningSourceControl/Commit.cs
+++ applications/VersioningSourceControl/Commit.cs
@@ -4,6 +4,7 @@ namespace VersioningSourceControl
 {
     public class CommitSourceControl : ICommitSourceControl
     {
+        public string Id { get; set; }
         public string Committer { get ; set ; }
         public string Message { get ; set ; }
         public DateTimeOffset CommitedDate { get ; set ; }
--- applications/VersioningSourceControl/ICommit.cs
+++ applications/VersioningSourceControl/ICommit.cs
@@ -4,6 +4,7 @@ namespace VersioningSourceControl
 {
     public interface ICommitSourceControl
     {
+        string Id { get; set; }
         string Committer { get; set; }
         string Message { get; set; }
         DateTimeOffset CommitedDate { get; set; }
